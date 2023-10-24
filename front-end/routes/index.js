4d23d2bf70dc01f7d75f9f0e9ba7f9e1dabebb2d
devRichard06 <richard.sa@aluno.ifsp.edu.br>
2023-10-24 16:35:37 -0300

    paginas do site em ejs

--- front-end/routes/index.js
+++ front-end/routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/atendimento', function(req, res, next) {
+  res.render('site/pages/Atendimento', {  layout: 'layouts/layout.ejs'  });
+});
+
+router.get('/esquecisenha', function(req, res, next) { 
+  res.render('site/pages/Esqueci-minha-senha', {  layout: 'layouts/layout1.ejs'  });
+});
+
+router.get('/carterinha', function(req, res, next) {
+  res.render('site/pages/Faça-sua-carterinha', {  layout: 'layouts/layout.ejs'  });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('site/pages/index', {  layout: 'layouts/layout.ejs'  });
+});
+
+router.get('/logar', function(req, res, next) {
+  res.render('site/pages/login', {  layout: 'layouts/layout1.ejs'  });
+});
+
+routget('/recaregar', function(req, res, next) {
+  res.render('site/pages/recarga-cartao', {  layout: 'layouts/layout.ejs'  });
+});
+
+router.get('/sobre', function(req, res, next) {
+  res.render('site/pages/Sobre-nos', {  layout: 'layouts/layout.ejs'  });
+});
+
+
+
 module.exports = router;
