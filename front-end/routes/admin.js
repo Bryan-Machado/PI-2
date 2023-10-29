17c8e5ba6491a5c1ea989fea762247ab26cc394f
Bryan Machado <00001101560393sp@al.educacao.sp.gov.br>
2023-10-29 15:05:10 -0300

    admin em express ejs. fazer rotas

--- front-end/routes/admin.js
+++ front-end/routes/admin.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.render('admin/index', { title: 'Express' });
+});
+
 router.get("/cadastrar-cliente", function (req, res, next) {
-  res.render("admin/gentelella-master/production/form_validation");
+  res.render("admin/form_validation");
 });
 
 router.get("/cadastrar-motorista", function (req, res, next) {
