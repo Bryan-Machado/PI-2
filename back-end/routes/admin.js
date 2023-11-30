3032daa78df0eca0ad4a4f3167b221f0078a4bbd
Bryan Machado <00001101560393p@al.educacao.sp.gov.br>
2023-11-30 17:03:43 -0300

    comeco login admin

---
+++ back-end/routes/admin.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+
+const { PrismaClient, Prisma } = require('@prisma/client');
+const prisma = new PrismaClient({errorFormat: 'minimal'});
+const bcrypt = require('bcryptjs');
+const { generateAccessToken, authenticateToken } = require('../auth');
+
+
+
+function exceptionHandler(e) {
+  let error = {
+    code: 500,
+    message: 'internal server error'
+  }
+  
+  if (
+    e instanceof Prisma.PrismaClientKnownRequestError ||
+    e instanceof Prisma.PrismaClientValidationError
+  ) {
+    error.code = 400;
+    error.message = e.message
+  }
+
+  return error
+}
+
+// POST /api/admin/login
+router.post('/login', async(req, res) => {
+    try {
+      const dados = req.body;
+      if (!'senha' in dados || !'cpf' in dados) {
+        return res.status(401).json({
+          error: "CPF e senha são obrigatórios"
+        });
+      }
+      const admin = await prisma.cliente.findUniqueOrThrow({
+        where: {
+          cpf: dados.cpf
+        }
+      });
+      const passwordCheck = await bcrypt.compare(
+        dados.senha, admin.senha
+      );
+      if (!passwordCheck) {
+        return res.status(401).json({
+          error: "CPF e/ou senha incorreto(s)"
+        });
+      }
+      delete admin.senha;
+      const jwt = generateAccessToken(admin);
+      admin.accessToken = jwt;
+      res.json(admin);
+    }
+    catch (exception) {
+      console.log(exception)
+      let error = exceptionHandler(exception);
+      return res.status(error.code).json({
+        error: error.message
+      })
+    }
+  });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
